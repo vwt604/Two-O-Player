1de106d6697998ddb47a5b5c201f9c9cfec94e27
Vanessa Y <vyeung.wt@gmail.com>
2021-03-30 04:51:05 +0000

    initialize questions

--- questions.rb
+++ questions.rb
@@ -1,4 +1,15 @@
 class Questions
 
+  attr_accessor :correct
+
+  def initialize(current_player)
+    @num_1 = rand(20)
+    @num_2 = rand(20)
+    @answer = @num_1 + @num_2
+
+    puts "#{current_player}: What does #{@num_1} plus #{@num_2} equal?"
+    ask_question
+
+  end
 
 end
