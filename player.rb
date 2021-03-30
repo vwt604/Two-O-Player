527a119738f5254dd940580c61420aeff8ab6057
Vanessa Y <vyeung.wt@gmail.com>
2021-03-30 04:47:20 +0000

    add player class

--- player.rb
+++ player.rb
@@ -1,5 +1,20 @@
 class Player 
+  
+  attr_accessor :lives
+  attr_reader :name
 
+  def initialize
+    @name = name
+    @lives = 3
+  end
+
+  def lose_life
+    @lives -= 1
+  end
+
+  def dead
+    @lives === 0
+  end
 
 end
 
